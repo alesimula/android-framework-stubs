8183f05fa1dcf8e1aed8ce970018dc6440459ddd
Alex Sim <alessandro.simula@lynxspa.com>
2026-03-25 20:44:56 +0100

    FRAMEWORK-16 - fix missing classes

---
+++ framework/android/internal/modules/utils/build/SdkLevel.java
@@ -0,0 +1,11 @@
+package android.internal.modules.utils.build;
+
+public final class SdkLevel {
+    public static boolean isAtLeastR() { return false; }
+    public static boolean isAtLeastS() { return false; }
+    public static boolean isAtLeastSv2() { return false; }
+    public static boolean isAtLeastT() { return false; }
+    public static boolean isAtLeastU() { return false; }
+    public static boolean isAtLeastV() { return false; }
+    public static boolean isAtLeastB() { return false; }
+}
